71c0144ff060a799b0e28a4d88def99c3ccd8976
Abhay <109281829+s1282001@users.noreply.github.com>
2024-10-17 13:11:30 +0900

    Update OscillatorUI.vue

--- app/src/components/OscillatorUI.vue
+++ app/src/components/OscillatorUI.vue
@@ -62,7 +62,6 @@ export default {
     oscTypeName(i) {
       const key = Object.keys(this.oscTypes).find(key => this.oscTypes[key].index == i - 1)
       return this.oscTypes[key].name
-    }
   },
   midiFrequency(midiNote) {
       return 440 * Math.pow(2, (midiNote - 69) / 12); 
@@ -70,7 +69,6 @@ export default {
   changeFrequency(midiNote) {
       this.logFreq = Math.log(this.midiFrequency(midiNote)); 
       this.frequencyChanged(); 
-    }
   },
 }
 </script>

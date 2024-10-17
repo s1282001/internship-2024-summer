aed1fc4c9b2d3e9995d7baf534f3ad5bb35d4141
Abhay <109281829+s1282001@users.noreply.github.com>
2024-10-17 14:55:28 +0900

    Update OscillatorUI.vue

--- app/src/components/OscillatorUI.vue
+++ app/src/components/OscillatorUI.vue
@@ -8,21 +8,15 @@ import parameterDescriptor from "../parameterDescriptor.js";
     <div class="param" id="oscType">
       <h5>{{ params.oscType.name }}</h5>
       <select v-model="oscType" @change="oscTypeChanged">
-        <option v-for="i in Object.keys(oscTypes).length" v-bind:value="i - 1" :key="i">
+        <option v-for="i in Object.keys(oscTypes).length" v-bind:value="i - 1">
           {{ oscTypeName(i) }}
         </option>
       </select>
     </div>
     <div class="param" id="oscFrequency">
       <h5>{{ params.frequency.name }}</h5>
-      <input
-        type="range"
-        :min="minLogFreq"
-        :max="maxLogFreq"
-        :step="(maxLogFreq - minLogFreq) / 10000"
-        v-model="logFreq"
-        @input="frequencyChanged"
-      />
+      <input type="range" :min="minLogFreq" :max="maxLogFreq" :step="(maxLogFreq - minLogFreq) / 10000"
+        v-model="logFreq" @input="frequencyChanged" />
       <div>{{ frequency }} Hz</div>
     </div>
   </div>
@@ -36,28 +30,36 @@ export default {
     return {
       params: parameterDescriptor.parameters,
       oscTypes: parameterDescriptor.oscTypes,
+
       oscType: parameterDescriptor.parameters.oscType.defaultValue,
-      frequency: parameterDescriptor.parameters.frequency.defaultValue,
+      frequency: parameterDescriptor.parameters.frequency.defaultValue, // Initialize frequency here
+
+      // for log scale slider
       logFreq: Math.log(parameterDescriptor.parameters.frequency.defaultValue),
       minLogFreq: Math.log(parameterDescriptor.parameters.frequency.minValue),
       maxLogFreq: Math.log(parameterDescriptor.parameters.frequency.maxValue),
     };
   },
+  watch: {
+    frequency(newVal) {
+      this.logFreq = Math.log(newVal); // Update logFreq when frequency changes
+    },
+  },
   methods: {
-    oscTypeChanged() {
+    oscTypeChanged(event) {
       const param = { id: this.params.oscType.id, value: this.oscType };
       this.$emit("parameterChanged", param);
     },
-    frequencyChanged() {
+    frequencyChanged(event) {
       const frequency = Math.round(Math.exp(this.logFreq));
-      if (frequency !== this.frequency) {
-        this.frequency = frequency;
+      if (frequency != this.frequency) {
+        this.frequency = frequency; // Update internal frequency
         const param = { id: this.params.frequency.id, value: this.frequency };
         this.$emit("parameterChanged", param);
       }
     },
     oscTypeName(i) {
-      const key = Object.keys(this.oscTypes).find(key => this.oscTypes[key].index === i - 1);
+      const key = Object.keys(this.oscTypes).find((key) => this.oscTypes[key].index == i - 1);
       return this.oscTypes[key].name;
     },
   },
@@ -65,3 +67,4 @@ export default {
 </script>
 
 <style scoped></style>
+

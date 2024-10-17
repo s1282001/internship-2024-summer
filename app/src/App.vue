6cb8204d02022de70baadea474e5acb46a7ad6a2
Abhay <109281829+s1282001@users.noreply.github.com>
2024-10-17 14:47:29 +0900

    Update App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -52,7 +52,7 @@ export default {
       synthesizer: null,
       analyser: null,
       waveDataSize: 512,
-      params: parameterDescriptor.parameters
+      params: parameterDescriptor.parameters,
     }
   },
   methods: {
@@ -64,9 +64,9 @@ export default {
       console.log("start")
       this.isStarted = true
       window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      const context = new AudioContext()
-      this.sampleRate = context.sampleRate
-      this.context = context
+      const context = new AudioContext();
+      this.sampleRate = context.sampleRate;
+      this.context = context;
 
       let options = {
         processorOptions: {
@@ -77,50 +77,46 @@ export default {
         outputChannelCount: [1]
       }
       context.audioWorklet.addModule(MyWorkletProcessorUrl).then(() => {
-        this.synthesizer = new AudioWorkletNode(context, 'synthesizer-worklet', options)
+        this.synthesizer = new AudioWorkletNode(context, 'synthesizer-worklet', options);
 
-        console.log(this.synthesizer)
+        console.log(this.synthesizer);
 
         this.analyser = this.context.createAnalyser();
         this.analyser.maxDecibels = 0;
         this.analyser.fftSize = this.waveDataSize;
-        this.synthesizer.connect(this.analyser).connect(context.destination)
+        this.synthesizer.connect(this.analyser).connect(context.destination);
 
-        setInterval(this.draw, 1000 / 10) //  オシロスコープ描画用タイマー
+        setInterval(this.draw, 1000 / 10); //  オシロスコープ描画用タイマー
       })
     },
     onParameterChanged(value) {
-      const data = { type: "param", value: value }
-      this.synthesizer.port.postMessage(data)
+      const data = { type: "param", value: value };
+      this.synthesizer.port.postMessage(data);
     },
     noteOn() {
-      console.log("noteOn")
-      const data = { type: "noteOn", value: true }
-      this.synthesizer.port.postMessage(data)
+      console.log("noteOn");
+      const data = { type: "noteOn", value: true };
+      this.synthesizer.port.postMessage(data);
     },
     noteOff() {
-      const data = { type: "noteOn", value: false }
-      this.synthesizer.port.postMessage(data)
+      const data = { type: "noteOn", value: false };
+      this.synthesizer.port.postMessage(data);
     },
     setupMidi() {
       MidiHandler.init(); // Web MIDIのセットアップ
       MidiHandler.setHandleMidiCallback(this.processMidiMessage); // processMidiMessageをMIDI受信時のコールバックに設定
     },
     processMidiMessage(message) {
-      // console.log(message);
       const data = message.data; // データを取得 Uint8Array（8bit符号なし整数値の配列)
       const [status, data1, data2] = data; // 各要素を変数に格納　先頭がステータスバイト、それ以降がデータバイト
-      //各データバイトは1byte（8bit）だが先頭の1bitはデータバイトであることを示すフラグで常に０なので得られる値は7bit(0~127)である
-      console.log(`status-byte: ${status}, data-byte-1: ${data1}, data-byte-2: ${data2}`)
+      console.log(`status-byte: ${status}, data-byte-1: ${data1}, data-byte-2: ${data2}`);
 
       switch (status >> 4) {
         case 0x8:  // 8n hex(nはMIDIチャンネル)はノートオフを指す
-          //  ノートオフメッセージの場合も、data1：ノート番号、data2：ベロシティ
           this.midiNoteOff(data1, data2);
           break;
 
         case 0x9:  // 9n hex(nはMIDIチャンネル)はノートオンを指す
-          //  ノートオンメッセージの場合、data1：ノート番号、data2：ベロシティ
           this.midiNoteOn(data1, data2);
           break;
 
@@ -133,19 +129,18 @@ export default {
       this.noteOff();
     },
     midiNoteOn(noteNumber, velocity) {
-      // todo, support note number and velocity
-      this.midiFrequency(noteNumber);
-
+      this.currentFrequency = this.midiFrequency(noteNumber);
       this.noteOn();
     },
-    midiFrequency(noteNumber){
-      return 440*math.pow(2,((noteNumber-69)/12);
+    midiFrequency(noteNumber) {
+      return 440 * Math.pow(2, (noteNumber - 69) / 12);
     },
     draw() {
-      this.$refs.spectrum.drawSpectrum()
+      this.$refs.spectrum.drawSpectrum();
     }
   },
 }
 </script>
 
 <style scoped></style>
+

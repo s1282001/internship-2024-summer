043615780298ffa65fdf5a010c20b31f5473bbe5
Abhay <109281829+s1282001@users.noreply.github.com>
2024-10-17 14:34:04 +0900

    Update OscillatorUI.vue

--- app/src/components/OscillatorUI.vue
+++ app/src/components/OscillatorUI.vue
@@ -1,90 +1,70 @@
 <script setup>
-import { ref, watch, defineProps, defineEmits } from 'vue';
-import parameterDescriptor from "../parameterDescriptor.js";
-
-// Define props
-const props = defineProps({
-  frequency: {
-    type: Number,
-    required: true,
-  },
-});
-
-// Define emits
-const emit = defineEmits(['parameterChanged']);
-
-// Define reactive properties
-const params = parameterDescriptor.parameters;
-const oscTypes = parameterDescriptor.oscTypes;
-
-const oscType = ref(parameterDescriptor.parameters.oscType.defaultValue);
-
-// For log scale slider
-const logFreq = ref(Math.log(props.frequency));
-const minLogFreq = ref(Math.log(parameterDescriptor.parameters.frequency.minValue));
-const maxLogFreq = ref(Math.log(parameterDescriptor.parameters.frequency.maxValue));
-
-// Watch for frequency changes to update logFreq
-watch(() => props.frequency, (newVal) => {
-  logFreq.value = Math.log(newVal);
-});
-
-// Emit frequency change
-const frequencyChanged = () => {
-  const freq = Math.round(Math.exp(logFreq.value));
-  if (freq !== props.frequency) {
-    emit("parameterChanged", { id: params.frequency.id, value: freq });
-  }
-};
-
-const oscTypeChanged = () => {
-  emit("parameterChanged", { id: params.oscType.id, value: oscType.value });
-};
-
-const oscTypeName = (i) => {
-  const key = Object.keys(oscTypes).find(key => oscTypes[key].index === i - 1);
-  return oscTypes[key].name;
-};
+import parameterDescriptor from "../parameterDescriptor.js"
 </script>
 
 <template>
-  <div> <!-- Added a single root element -->
-    <h3 class="section-title">Oscillator</h3>
-    <div class="section">
-      <div class="param" id="oscType">
-        <h5>{{ params.oscType.name }}</h5>
-        <select v-model="oscType" @change="oscTypeChanged">
-          <option v-for="i in Object.keys(oscTypes).length" :key="i" :value="i - 1">
-            {{ oscTypeName(i) }}
-          </option>
-        </select>
-      </div>
-      <div class="param" id="oscFrequency">
-        <h5>{{ params.frequency.name }}</h5>
-        <input type="range" :min="minLogFreq" :max="maxLogFreq" :step="(maxLogFreq - minLogFreq) / 10000"
-          v-model="logFreq" @input="frequencyChanged" />
-        <div>{{ Math.round(Math.exp(logFreq)) }} Hz</div>
+  <h3 class="section-title">Oscillator</h3>
+  <div class="section">
+    <div class="param" id="oscType">
+      <h5>
+        {{ params.oscType.name }}
+      </h5>
+      <select v-model="oscType" @change="oscTypeChanged">
+        <option v-for="i in Object.keys(oscTypes).length" v-bind:value="i - 1">
+          {{ oscTypeName(i) }}
+        </option>
+      </select>
+    </div>
+    <div class="param" id="oscFrequency">
+      <h5>
+        {{ params.frequency.name }}
+      </h5>
+      <input type="range" :min="minLogFreq" :max="maxLogFreq" :step="(maxLogFreq - minLogFreq) / 10000"
+        v-model="logFreq" @input="frequencyChanged" />
+      <div>
+        {{ frequency }} Hz
       </div>
     </div>
-  </div> <!-- Single root element -->
+  </div>
 </template>
 
-<style scoped>
-.section {
-  margin: 16px;
-}
+<script>
+export default {
+  name: "OscillatorUI",
+  emits: ["parameterChanged"],
+  data() {
+    return {
+      params: parameterDescriptor.parameters,
+      oscTypes: parameterDescriptor.oscTypes,
 
-.param {
-  margin-bottom: 16px;
-}
+      oscType: parameterDescriptor.parameters.oscType.defaultValue,
+      frequency: parameterDescriptor.parameters.frequency.defaultValue,
 
-.section-title {
-  font-size: 1.5rem;
-  margin-bottom: 12px;
+      // for log scale slider
+      logFreq: Math.log(parameterDescriptor.parameters.frequency.defaultValue),
+      minLogFreq: Math.log(parameterDescriptor.parameters.frequency.minValue),
+      maxLogFreq: Math.log(parameterDescriptor.parameters.frequency.maxValue),
+    }
+  },
+  methods: {
+    oscTypeChanged(event) {
+      const param = { id: this.params.oscType.id, value: this.oscType }
+      this.$emit("parameterChanged", param)
+    },
+    frequencyChanged(event) {
+      const frequency = Math.round(Math.exp(this.logFreq));
+      if (frequency != this.frequency) {
+        this.frequency = frequency
+        const param = { id: this.params.frequency.id, value: this.frequency }
+        this.$emit("parameterChanged", param)
+      }
+    },
+    oscTypeName(i) {
+      const key = Object.keys(this.oscTypes).find(key => this.oscTypes[key].index == i - 1)
+      return this.oscTypes[key].name
+    }
+  },
 }
+</script>
 
-h5 {
-  margin: 8px 0;
-}
-</style>
-
+<style scoped></style>
